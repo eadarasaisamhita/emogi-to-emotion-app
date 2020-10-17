e47c74ef0f090c245ead0cd5acc03e8b5d13be88
eadarasaisamhita <72115037+eadarasaisamhita@users.noreply.github.com>
2020-10-17 19:56:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -18,8 +18,42 @@ function take_snapshot()
 }
 
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://storage.googleapis.com/tm-model/5mxzZHkpx/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded!');
+}
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+   if(error) {
+       console.error(error)
+   }
+   else{
+       console.log(results);
+       document.getElementById("result_emotion_name").innerHTML=result[0].label;
+       document.getElementById("result_emotion_name").innerHTML=result[1].label;
+       if(results[0].label="happy"){
+           document.getElementById("update_emoji").innerHTML="&#128522;"
+       }
+       if(results[0].label="happy"){
+        document.getElementById("update_emoji").innerHTML="&#128532;"
+    }
+    if(results[0].label="happy"){
+        document.getElementById("update_emoji").innerHTML="&#128545;"
+    }
+    if(results[1].label="happy"){
+        document.getElementById("update_emoji").innerHTML="&#128522;"
+    }
+    if(results[1].label="happy"){
+     document.getElementById("update_emoji").innerHTML="&#128532;"
+ }
+ if(results[1].label="happy"){
+     document.getElementById("update_emoji").innerHTML="&#128545;"
+ }
+   }
 }
